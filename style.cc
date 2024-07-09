202b7c5fb20c439b5f0d9596e62c30948fe6d261
Kevin25501 <xxmuertte25501@gmail.com>
2024-07-08 22:48:05 -0500

    Add files via upload

---
+++ style.cc
@@ -0,0 +1,64 @@
+/* Estilo general de la página */
+body {
+    font-family: sans-serif;
+    margin: 0;
+    padding: 20px;
+    background-color: #f4f4f4;
+}
+
+/* Encabezado */
+header {
+    text-align: center;
+    margin-bottom: 40px;
+}
+
+h1 {
+    font-size: 32px;
+    color: #333;
+}
+
+h2 {
+    font-size: 24px;
+    color: #666;
+    margin-bottom: 20px;
+}
+
+/* Secciones */
+section {
+    margin-bottom: 30px;
+}
+
+h2 {
+    margin-top: 0;
+}
+
+ul {
+    list-style: none;
+    padding: 0;
+}
+
+li {
+    margin-bottom: 10px;
+}
+
+/* Información personal */
+#informacion-personal li {
+    display: flex;
+    align-items: center;
+}
+
+#informacion-personal i {
+    margin-right: 10px;
+    color: #333;
+}
+
+/* Pie de página */
+footer {
+    text-align: center;
+    margin-top: 40px;
+}
+
+p {
+    font-size: 14px;
+    color: #666;
+}
